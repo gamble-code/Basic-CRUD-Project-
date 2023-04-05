7f9b8c0614e8c33d010227e8e5e62d8edafd5f9c
Krishna <kirankrsna7@outlook.com>
2023-04-05 11:41:37 +0530

    final commit

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -7,11 +7,6 @@ urlpatterns = [
     path('', views.data_input, name = 'home'),
     path('update/<int:id>/',views.update_data, name = 'update'),
     path('delete/<int:id>/',views.delete_data, name = 'delete'),
-    path('signup/',views.sign_up, name = 'signup'),
-    path('login/',views.log_in, name = 'login'),
-    path('profile/',views.user_profile, name = 'userprofile'),
-    path('logout/',views.user_logout, name = 'logout'),
-    path('changepassword/',views.user_changepassword, name = 'changepass'),
-    path('userdetail/<int:id>',views.user_detail, name = 'userdetail'),
+    
     
 ]
