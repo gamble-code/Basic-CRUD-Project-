9592ca9ed6f849958f582fdbb2ae3a72a30ecd7a
Krishna <kirankrsna7@outlook.com>
2023-04-05 11:47:21 +0530

    final commit

--- crudapp/forms.py
+++ crudapp/forms.py
@@ -14,29 +14,3 @@ class Form(forms.ModelForm):
                    'email': forms.EmailInput(attrs={'class':'form-control' ,'placeholder' : 'enter your email-id'}),
                    'expertise': forms.TextInput(attrs={'class':'form-control' ,'placeholder' : 'enter software language you know'}),}
 
-class SignupForm(UserCreationForm):
-    class Meta():
-        password2 = forms.CharField(label = 'Confirm- Password', widget= forms.PasswordInput(attrs={'class':'form-control'}))
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
-        labels = {'username': 'Username', 'first_name': 'First Name', 'last_name' : 'Last Name', 'email' : 'Email-Id'}
-        widgets =  {'username': forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'enter a unique username'}),
-          'first_name': forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'enter your first name'}),
-        'last_name': forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'enter your last name'}),
-        'email': forms.EmailInput(attrs={'class':'form-control', 'placeholder' : 'enter your email-id'}),}
-        
-
-class EditUserProfileForm(UserChangeForm):
-    password =None
-    class Meta:
-        model = User
-        fields =['username', 'first_name','last_name','email']
-        labels = {'username': 'Username', 'first_name': 'First Name', 'last_name' : 'Last Name', 'email' : 'Email-Id'}
-
-
-class EditAdminProfileForm(UserChangeForm):
-    password = None
-    class Meta:
-        model = User
-        fields ='__all__'
-        labels = {'username': 'Username', 'first_name': 'First Name', 'last_name' : 'Last Name', 'email' : 'Email-Id'}
